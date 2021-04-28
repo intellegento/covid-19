8ba6c2762eb47ec77e2fa6121d3cf4a5bc0ebeb5
intellegento <61728197+intellegento@users.noreply.github.com>
2021-04-29 00:15:45 +0600

    rewritten webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,71 @@
-const path = require('path');
+const path = require('path')
+
+const webpack = require('webpack')
+
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devServer: {
-    contentBase: './dist',
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-     {
-       test: /\.(woff|woff2|eot|ttf|otf)$/i,
-       type: 'asset/resource',
-     },
+    entry: {
+        index: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash].js',
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: process.env.PORT,
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'WEBPACK',
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+
+        // clean after build
+        new CleanWebpackPlugin(),
+
+        // Only update what has changed on hot reload
+        new webpack.HotModuleReplacementPlugin(),
     ],
-  },
-};
+
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+
+            // Images
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: 'asset/resource',
+            },
+
+            // Fonts and SVGs
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+
+            // CSS, PostCSS, and Sass
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+}
