a393e07c550e51cc41d0c6674e9beb34ebe7b510
intellegento <61728197+intellegento@users.noreply.github.com>
2021-06-09 23:37:53 +0600

    change directory folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,70 @@
-const path = require('path')
+const path = require("path");
 
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 // plugins
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname, './src/index.js'),
-    },
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+  },
 
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].[hash].js',
-    },
+  output: {
+    path: path.resolve(__dirname, "./docs"),
+    filename: "[name].[hash].js",
+  },
 
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, './dist'),
-        open: true,
-        compress: true,
-        hot: true,
-        port: process.env.PORT,
-    },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./docs"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: process.env.PORT,
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'WEBPACK',
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "WEBPACK",
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
 
-        // clean after build
-        new CleanWebpackPlugin(),
+    // clean after build
+    new CleanWebpackPlugin(),
 
-        // Only update what has changed on hot reload
-        new webpack.HotModuleReplacementPlugin(),
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+
+      // Images
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource",
+      },
+
+      // Fonts and SVGs
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+
+      // CSS, PostCSS, and Sass
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
     ],
-
-    module: {
-        rules: [
-            // JavaScript
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-
-            // Images
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-                type: 'asset/resource',
-            },
-
-            // Fonts and SVGs
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
-
-            // CSS, PostCSS, and Sass
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-            },
-        ],
-    },
-}
+  },
+};
