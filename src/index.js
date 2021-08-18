a333b7c8bb9bd181728840ae7dc990a1d914e1bf
intellegento <61728197+intellegento@users.noreply.github.com>
2021-08-18 19:19:51 +0600

    add interactive to site

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import './styles/main.scss'
+import "./styles/main.scss";
 
-const bootstrap = require('bootstrap')
+const bootstrap = require("bootstrap");
+
+let virus = document.getElementById("virus");
+let virus2 = document.getElementById("virus-2");
+let virus3 = document.getElementById("virus-3");
+let virus4 = document.getElementById("virus-3");
+let girlHeader = document.getElementById("girl-header");
+
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  virus.style.width = value * 10 + "px";
+  virus2.style.opacity = value * 1 + "%";
+  virus.style.opacity = value + 1 + "%";
+});
